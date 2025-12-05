f4567c6a6c98cdf3ec2a0373f007c379a8e2fcfe
Jakub Górski <jakubg1527@proton.me>
2025-12-05 22:45:37 +0100

    Add config from astronvim

---
+++ lua/config/spell-checker.lua
@@ -0,0 +1,4 @@
+-- Add spell checker
+vim.opt.spell = true
+-- When there is lack of .spl file download it from there: https://ftp.nluug.nl/vim/runtime/spell/ and place it in: ~/.local/share/nvim/site/spell/
+vim.opt.spelllang = { "pl", "en" }
---
+++ lua/config/vim-settings.lua
@@ -0,0 +1,4 @@
+# Text wrapping
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.breakindent = true
