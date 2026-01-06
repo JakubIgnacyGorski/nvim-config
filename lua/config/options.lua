e00ab4ba85b4dcdb9677e365108e930b6d310727
Jakub Górski <jakubg1527@gmail.com>
2026-01-06 11:48:41 +0100

    Repair spell checker

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,9 @@
 -- Add any additional options here
 vim.opt.relativenumber = true
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+vim.opt.wrap = true
+
+vim.opt.spell = true
+-- When there is lack of .spl file download it from there: https://ftp.nluug.nl/vim/runtime/spell/ and place it in: ~/.local/share/nvim/site/spell/
+-- mkdir -p ~/.local/share/nvim/site/spell && curl -fLo ~/.local/share/nvim/site/spell/pl.utf-8.spl https://ftp.nluug.nl/vim/runtime/spell/pl.utf-8.spl
+vim.opt.spelllang = { "pl", "en" }
