100b0864571b77c2966b0fa1e3cf079706dae539
Jakub Górski <jakubg1527@gmail.com>
2026-01-10 14:36:32 +0100

    Simpler npm install command

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,6 @@
 -- If there is red Node.js version and nothing happends refer to: https://github.com/iamcco/markdown-preview.nvim/issues/695
 -- Solution
--- cd ~/.local/share/nvim/lazy/markdown-preview.nvim
--- npm install
+-- cd ~/.local/share/nvim/lazy/markdown-preview.nvim && npm install
 return {
   {
     "iamcco/markdown-preview.nvim",
