eff18039c7b1d9b37f90314af2631b74d7e9f0ae
Jakub Górski <jakubg1527@gmail.com>
2026-01-18 12:08:09 +0100

    Add carbon now sh

---
+++ lua/plugins/vim-carbon-now-sh.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "kristijanhusak/vim-carbon-now-sh",
+    keys = {
+      { "<leader>cv", "<cmd>CarbonNowSh<CR>", mode = "n", desc = "CarbonNowSh" },
+      { "<leader>cv", ":CarbonNowSh<CR>", mode = "v", desc = "CarbonNowSh selected code" },
+    },
+  },
+}
