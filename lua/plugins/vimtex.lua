97b640b9a4edc7dbdaeb6689a0b308097a3e3c65
Jakub Górski <jakubg1527@gmail.com>
2026-01-20 21:58:28 +0100

    Update latexmk-podman to better dir

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,7 +5,7 @@ return {
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_compiler_latexmk = {
         -- latexmk-podman example with official texlive image
-        --  echo '#!/bin/bash\n\npodman run --rm --network=none --security-opt=no-new-privileges -v "$(pwd)":/data -w /data "registry.gitlab.com/islandoftex/images/texlive:latest" latexmk "$@"' | sudo tee /usr/bin/latexmk-podman && sudo chmod +x /usr/bin/latexmk-podman
+        --  echo '#!/bin/bash\n\npodman run --rm --network=none --security-opt=no-new-privileges -v "$(pwd)":/data -w /data "registry.gitlab.com/islandoftex/images/texlive:latest" latexmk "$@"' | sudo tee /usr/local/bin/latexmk-podman && sudo chmod +x /usr/local/bin/latexmk-podman
         executable = "latexmk-podman",
       }
     end,
