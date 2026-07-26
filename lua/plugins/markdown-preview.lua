b7b5f7c9ad00b49f671ab6a07a54cd8b09c75faa
Jakub Górski <jakubg1527@proton.me>
2026-07-26 15:21:54 +0200

    Squash merge lazyvim-extras into lazyvim

--- lua/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
--- If there is red Node.js version and nothing happends refer to: https://github.com/iamcco/markdown-preview.nvim/issues/695
--- Solution
--- cd ~/.local/share/nvim/lazy/markdown-preview.nvim && npm install
-return {
-  {
-    "iamcco/markdown-preview.nvim",
-    latex = { enabled = false },
-  },
-}
